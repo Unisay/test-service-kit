c0e7bc78be8d69a767f8432e2ca02577c585d220
Yuriy Lazaryev <Yuriy.Lazaryev@zalando.de>
2016-02-10 11:45:10 +0100

    Remove oauth_scope as its not needed often

--- src/main/scala/org/zalando/test/kit/service/WebServiceConfig.scala
+++ src/main/scala/org/zalando/test/kit/service/WebServiceConfig.scala
@@ -1,3 +1,3 @@
 package org.zalando.test.kit.service
 
-case class WebServiceConfig(port: Int, serviceName: String, path: String, url: String, oauthScope: String)
+case class WebServiceConfig(port: Int, serviceName: String, path: String, url: String)
